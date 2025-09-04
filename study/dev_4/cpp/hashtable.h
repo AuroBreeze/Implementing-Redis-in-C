0c5f7446029e77c6084aacb17367bd75cc008a9c
AuroBreeze <1732373074@qq.com>
2025-09-04 16:07:28 +0800

    feat(dev_4): 添加 AVL 树和哈希表的实现
    - 实现了 AVL 树的基本操作，包括插入、删除和查找
    - 实现了哈希表的基本操作，包括插入、删除和查找
    - 添加了相应的头文件和实现文件

---
+++ study/dev_4/cpp/hashtable.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <cstddef>   // for size_t
+#include <cstdint>   // for uint64_t
+
+struct HNode{
+    HNode *next;
+    uint64_t hcode = 0;
+};
+
+struct HTab{
+    HNode* *tab = nullptr;
+    size_t mask = 0;
+    size_t size = 0;
+};
+
+struct HMap
+{
+    HTab newer;
+    HTab older;
+    size_t migrate_pos = 0;
+};
+
+HNode *hm_lookup(HMap* hmap,HNode* key,bool (*eq)(HNode* , HNode*));
+void hm_insert(HMap* hmap,HNode* node);
+HNode *hm_delete(HMap* hmap,HNode* key,bool (*eq)(HNode* , HNode*));
+void hm_clear(HMap* hmap);
+size_t hm_size(HMap* hmap);
+// invoke the callback on each node until it returns false
+void hm_foreach(HMap* hmap,bool (*f)(HNode*, void*), void* arg);
